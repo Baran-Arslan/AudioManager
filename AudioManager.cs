85833826659e5cf2d2d858de71c6e585bec35351
Baran Arslan <130396326+Baran-Arslan@users.noreply.github.com>
2023-12-30 16:17:39 +0100

    Add files via upload

--- AudioManager.cs
+++ AudioManager.cs
@@ -1,77 +1,33 @@
-using System.Collections;
 using UnityEngine;
-using UnityEngine.Pool;
 
-public class AudioManager : MonoBehaviour
+namespace iCareGames.Common.Core.AudioSystem
 {
-    private static AudioManager Instance;
-    private static ObjectPool<AudioSource> sfxSourcePool;
-    [SerializeField, Tooltip("You can add AudioMixer to your prefab and make prefab 3D")]
-    private AudioSource sfxSourcePrefab;
+    [RequireComponent(typeof(AudioController))]
+    public class AudioManager : MonoBehaviour {
+        private static AudioController _audioController;
 
-    [SerializeField] private SoundSO soundSO;
-    public static SoundSO Sounds;
+        private void Awake() {
+            _audioController = GetComponent<AudioController>();
+        }
 
-    private static Transform mainCamera;
-
-
-    private void Awake()
-    {
-        Instance = this;
-
-
-        Sounds = soundSO;
-
-        sfxSourcePool = new ObjectPool<AudioSource>
-            (() =>
-            {//Create
-                AudioSource audioSource = Instantiate(sfxSourcePrefab);
-                return audioSource;
-            }
-            , audioSource =>
-            {//On Take
-                audioSource.gameObject.SetActive(true); 
-            }
-            , audioSource =>
-            {//On Return
-                audioSource.gameObject.SetActive(false);
-            }
-            , audioSource =>
-            {//On Destroy
-               Destroy(audioSource.gameObject);
-            }
-            , true, 5, 100
-            );
-
-        mainCamera = Camera.main.transform;
-    }
-    
-
-    public static void PlaySFX(SoundData soundData, Vector3 playPosition = default(Vector3))
-    {
-        if (playPosition == default(Vector3))
-            playPosition = mainCamera.position;
-
-
-        AudioSource audioSource = sfxSourcePool.Get();
-        audioSource.transform.position = playPosition;
-        audioSource.clip = soundData.clip;
-        audioSource.volume = soundData.volume;
-        audioSource.Play();
-
-        float clipLength = soundData.clip.length;
-
-        Instance.StartCoroutine(ReturnToPool(audioSource, clipLength));
-    }
-
-    public static void PlaySFX(SoundData[] soundDataArray, Vector3 playPosition = default(Vector3))
-    {
-        SoundData randomSoundData = soundDataArray[Random.Range(0, soundDataArray.Length)];
-        PlaySFX(randomSoundData, playPosition);
-    }
-    private static IEnumerator ReturnToPool(AudioSource audioSource, float delay)
-    {
-        yield return new WaitForSeconds(delay);
-        sfxSourcePool.Release(audioSource);
+        public static void PlaySfx(AudioSO audioData, Vector3 playPosition = default, bool is2D = true, float smoothVolumeTime = 0, bool canAddToDictionary = false) {
+            _audioController.PlaySfx(audioData, playPosition, is2D, smoothVolumeTime, canAddToDictionary);
+        }
+        public static void StopSfx(AudioSO audioData, float smoothVolumeTime = 0) {
+            _audioController.StopSfx(audioData, smoothVolumeTime);
+        }
+        public static void PlayLoop(AudioSO audioData, Vector3 playPosition = default, bool is2D = true, float smoothVolumeTime = 0) {
+            _audioController.PlayLoop(audioData, playPosition, is2D, smoothVolumeTime);
+        }
+        public static void StopLoop(AudioSO audioData, float smoothVolumeTime = 0) {
+            _audioController.StopLoop(audioData, smoothVolumeTime);
+        }
+        public static void PlayMusic(AudioSO audioData) {
+            _audioController.PlayMusic(audioData);
+        }
+        public static void StopMusic(AudioSO audioData, float smoothVolumeTime = 0) {
+            _audioController.StopMusic(audioData, smoothVolumeTime);
+        }
+        
     }
 }
---
+++ AudioSO.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace iCareGames.Common.Core.AudioSystem {
+    [CreateAssetMenu(fileName = "AudioSO", menuName = "iCareGames/AudioSystem/AudioSO")]
+    public class AudioSO : ScriptableObject {
+        [SerializeField] private AudioClip[] Clips;
+        public AudioClip Clip => Clips[Random.Range(0, Clips.Length)];
+        
+        [Range(0, 1f)]
+        public float DefaultVolume = 1;
+        public float DefaultPitch { get; private set; } = 1f;
+        
+        
+        [Range(0, 256)]
+        public int Priority = 128;
+
+        [Range(0, 0.5f)] 
+        public float VolumeRandomness = 0;
+        [Range(0, 3)]
+        public float PitchRandomness = 0;
+    }
+}
